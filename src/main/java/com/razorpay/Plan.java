ddb37cd1b38bba4269881f9060273ea57f3fd748
hobbes09 <sourin.sutradhar@gmail.com>
2017-10-25 17:37:23 +0530

    created plan apis

---
+++ src/main/java/com/razorpay/Plan.java
@@ -0,0 +1,10 @@
+package com.razorpay;
+
+import org.json.JSONObject;
+
+public class Plan extends Entity {
+
+    public Plan(JSONObject jsonObject) {
+        super(jsonObject);
+    }
+}
---
+++ src/main/java/com/razorpay/PlanClient.java
@@ -0,0 +1,28 @@
+package com.razorpay;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class PlanClient extends ApiClient {
+
+    PlanClient(String auth) {
+        super(auth);
+    }
+
+    public Plan create(JSONObject request) throws RazorpayException {
+        return post(Constants.PLAN_CREATE, request);
+    }
+
+    public Plan fetch(String id) throws RazorpayException {
+        return get(String.format(Constants.PLAN_GET, id), null);
+    }
+
+    public List<Plan> fetchAll() throws RazorpayException {
+        return fetchAll(null);
+    }
+
+    public List<Plan> fetchAll(JSONObject request) throws RazorpayException {
+        return getCollection(Constants.PLAN_LIST, request);
+    }
+}
