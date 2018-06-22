06f2c2592bd1c342c4f5754a5193996c324f3fd5
Ankit Agarwal <ankit.agarwal@razorpay.com>
2018-06-22 14:17:05 +0530

    [TLS]: Adding MODERN_TLS which has support for 1.0, 1.1, 1.2

--- src/main/java/com/razorpay/ApiUtils.java
+++ src/main/java/com/razorpay/ApiUtils.java
@@ -11,14 +11,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.json.JSONObject;
 
-import okhttp3.CipherSuite;
 import okhttp3.ConnectionSpec;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.TlsVersion;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 class ApiUtils {
@@ -37,14 +35,8 @@ class ApiUtils {
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.NONE);
       }
 
-      ConnectionSpec connectionSpec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                                                        .tlsVersions(TlsVersion.TLS_1_2)
-                                                        .cipherSuites(CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                                                                      CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                                                                      CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
-                                                        .build();
       List<ConnectionSpec> connectionSpecs = new ArrayList<ConnectionSpec>();
-      connectionSpecs.add(connectionSpec);
+      connectionSpecs.add(ConnectionSpec.MODERN_TLS);
       
       client = new OkHttpClient.Builder()
                                .readTimeout(60, TimeUnit.SECONDS)
