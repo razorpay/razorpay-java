3ccf89b5c45ad758342e7a9bbf8c3573d3fa65e3
jitendra-1217 <jitendraojha1217@gmail.com>
2018-08-21 20:53:45 +0530

    invoice: Adds cancel endpoint

--- src/main/java/com/razorpay/Constants.java
+++ src/main/java/com/razorpay/Constants.java
@@ -38,6 +38,7 @@ public class Constants {
   static final String INVOICE_CREATE = "invoices";
   static final String INVOICE_GET = "invoices/%s";
   static final String INVOICE_LIST = "invoices";
+  static final String INVOICE_CANCEL = "invoices/%s/cancel";
 
   static final String CARD_GET = "cards/%s";
 
--- src/main/java/com/razorpay/InvoiceClient.java
+++ src/main/java/com/razorpay/InvoiceClient.java
@@ -25,4 +25,8 @@ public class InvoiceClient extends ApiClient {
   public Invoice fetch(String id) throws RazorpayException {
     return get(String.format(Constants.INVOICE_GET, id), null);
   }
+
+  public Invoice cancel(String id) throws RazorpayException {
+    return post(String.format(Constants.INVOICE_CANCEL, id), null);
+  }
 }
