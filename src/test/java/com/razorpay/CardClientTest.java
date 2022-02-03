1c420f94e9ea1dbd6178e1accbb7b42db0a24ea6
ankitdas13 <ankit.das@razorpay.com>
2022-02-03 12:15:39 +0530

    added card testcase

---
+++ src/test/java/com/razorpay/CardClientTest.java
@@ -0,0 +1,32 @@
+package com.razorpay;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CardClientTest extends BaseTest{
+
+    @InjectMocks
+    private CardClient cardClientClient = new CardClient("test");
+
+    public static final String CARD_ID = "card_DZon6fd8J3IcA2";
+
+    @Test
+    public void fetch() throws RazorpayException {
+
+        String json = "{\n  \"id\": \"card_DZon6fd8J3IcA2\",\n  \"entity\": \"card\",\n  \"international\": false,\n  \"last4\": 1111,\n  \"name\": \"sample name\",\n  \"network\": \"Visa\",\n  \"type\": \"debit\"\n}";
+        try {
+            mockResponseFromExternalClient(json);
+            mockResponseHTTPCodeFromExternalClient(200);
+            Card fetch = cardClientClient.fetch(CARD_ID);
+            assertNotNull(fetch);
+            assertEquals(CARD_ID,fetch.get("id"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
