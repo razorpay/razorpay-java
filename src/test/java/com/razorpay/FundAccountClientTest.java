da26aa251732c2e7f4d868e0b20ecd1d1154b0ea
ankitdas13 <ankit.das@razorpay.com>
2022-02-08 10:25:56 +0530

    add static variable

--- src/test/java/com/razorpay/FundAccountClientTest.java
+++ src/test/java/com/razorpay/FundAccountClientTest.java
@@ -11,9 +11,9 @@ import static org.junit.Assert.*;
 public class FundAccountClientTest extends BaseTest{
 
     @InjectMocks
-    private FundAccountClient fundAccountClient = new FundAccountClient("test");
+    protected FundAccountClient fundAccountClient = new FundAccountClient(TEST_SECRET_KEY);
 
-    public static final String FUNDACCOUNT_ID = "fa_Aa00000000001";
+    private static final String FUNDACCOUNT_ID = "fa_Aa00000000001";
 
     @Test
     public void create() throws RazorpayException {
