5074a446e92de85739af7bb19d3fa801d8e55a94
ankitdas13 <ankit.das@razorpay.com>
2022-02-14 13:29:41 +0530

    add javadoc & exception

--- src/test/java/com/razorpay/ItemClientTest.java
+++ src/test/java/com/razorpay/ItemClientTest.java
@@ -12,68 +12,96 @@ import static org.junit.Assert.*;
 public class ItemClientTest extends BaseTest{
 
     @InjectMocks
-    private ItemClient itemClient = new ItemClient("test");
+    protected ItemClient itemClient = new ItemClient("test");
 
     public static final String ITEM_ID = "item_IJol6jPh1ummTK";
-
+    
+    /**
+     * Create item with basic details such as name and amount details
+     * @throws RazorpayException
+     */
     @Test
     public void create() throws RazorpayException {
         JSONObject request = new JSONObject("{\n  \"name\": \"Book English August\",\n  \"description\": \"An indian story, Booker prize winner.\",\n  \"amount\": 20000,\n  \"currency\": \"INR\"\n}");
-        String json = "{\n  \"entity\" : \"item\",\n  \"id\": \"item_IJol6jPh1ummTK\",\n  \"active\": true,\n  \"name\": \"Book English August\",\n  \"description\": \"An indian story, Booker prize winner.\",\n  \"amount\": 20000,\n  \"currency\": \"INR\"\n}";
+        String mockedResponseJson = "{\n  \"entity\" : \"item\",\n  \"id\": "+ITEM_ID+",\n  \"active\": true,\n  \"name\": \"Book English August\",\n  \"description\": \"An indian story, Booker prize winner.\",\n  \"amount\": 20000,\n  \"currency\": \"INR\"\n}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             Item item = itemClient.create(request);
             assertNotNull(item);
             assertEquals(ITEM_ID,item.get("id"));
+            assertEquals("item",item.get("entity"));
+            assertTrue(item.has("active"));
+            assertTrue(item.has("name"));
+            assertTrue(item.has("name"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
 
+    /**
+     * Retrieve the item details using item id.
+     * @throws RazorpayException
+     */
     @Test
     public void fetch() throws RazorpayException {
 
-        String json = "{\n  \"entity\": \"item\",\n  \"id\": \"item_7Oxp4hmm6T4SCn\",\n  \"active\": true,\n  \"name\": \"Book English August\",\n  \"description\": \"An indian story, Booker prize winner.\",\n  \"amount\": 20000,\n  \"currency\": \"INR\"\n}";
+        String mockedResponseJson = "{\n  \"entity\": \"item\",\n  \"id\": "+ITEM_ID+",\n  \"active\": true,\n  \"name\": \"Book English August\",\n  \"description\": \"An indian story, Booker prize winner.\",\n  \"amount\": 20000,\n  \"currency\": \"INR\"\n}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             Item fetch = itemClient.fetch(ITEM_ID);
             assertNotNull(fetch);
+            assertEquals(ITEM_ID,fetch.get("id"));
             assertEquals("item",fetch.get("entity"));
+            assertTrue(fetch.has("amount"));
+            assertTrue(fetch.has("currency"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
-
+    
+    /**
+     * Details of all the items can be retrieved.
+     * @throws RazorpayException
+     */
     @Test
     public void fetchAll() throws RazorpayException{
 
-        String json = "{\n  \"entity\": \"collection\",\n  \"count\": 1,\n  \"items\": [\n    {\n      \"id\": \"item_7OxoGnoxCuUKbo\",\n       \"entity\" : \"item\",\n      \"active\": true,\n      \"name\": \"Book  English August\",\n      \"description\": null,\n      \"amount\": 20000,\n      \"currency\": \"INR\"\n    }\n  ]\n}";
+        String mockedResponseJson = "{\n  \"entity\": \"collection\",\n  \"count\": 1,\n  \"items\": [\n    {\n      \"id\": \"item_7OxoGnoxCuUKbo\",\n       \"entity\" : \"item\",\n      \"active\": true,\n      \"name\": \"Book  English August\",\n      \"description\": null,\n      \"amount\": 20000,\n      \"currency\": \"INR\"\n    }\n  ]\n}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             List<Item> fetch = itemClient.fetchAll();
             assertNotNull(fetch);
-            assertEquals(true,fetch.get(0).has("entity"));
+            assertTrue(fetch.get(0).has("entity"));
+            assertTrue(fetch.get(0).has("id"));
+            assertTrue(fetch.get(0).has("active"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
-
+    
+    /**
+     * Update an item details using item id with object of that properties
+     * @throws RazorpayException
+     */
     @Test
     public void edit() throws RazorpayException{
 
         JSONObject request = new JSONObject("{\n  \"name\": \"Book Ignited Minds - Updated name!\",\n  \"description\": \"New descirption too. :).\",\n  \"amount\": 20000,\n  \"currency\": \"INR\",\n  \"active\": true\n}");
-        String json = "{\n   \"entity\" : \"item\",\n  \"id\": \"item_IJol6jPh1ummTK\",\n  \"active\": true,\n  \"name\": \"Book Ignited Minds - Updated name!\",\n  \"description\": \"New descirption too. :)\",\n  \"amount\": 15000,\n  \"currency\": \"INR\"\n}";
+        String mockedResponseJson = "{\n   \"entity\" : \"item\",\n  \"id\": "+ITEM_ID+",\n  \"active\": true,\n  \"name\": \"Book Ignited Minds - Updated name!\",\n  \"description\": \"New descirption too. :)\",\n  \"amount\": 15000,\n  \"currency\": \"INR\"\n}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             Item item = itemClient.edit(ITEM_ID,request);
             assertNotNull(item);
             assertEquals(ITEM_ID,item.get("id"));
+            assertEquals("item",item.get("entity"));
+            assertTrue(item.has("amount"));
+            assertTrue(item.has("description"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
 }
