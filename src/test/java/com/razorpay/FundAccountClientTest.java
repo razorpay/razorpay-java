b8d07db07586c7a37f66b11ff7ad71325161b984
ankitdas13 <ankit.das@razorpay.com>
2022-02-14 12:58:05 +0530

    add javadoc

--- src/test/java/com/razorpay/FundAccountClientTest.java
+++ src/test/java/com/razorpay/FundAccountClientTest.java
@@ -14,34 +14,42 @@ public class FundAccountClientTest extends BaseTest{
     protected FundAccountClient fundAccountClient = new FundAccountClient(TEST_SECRET_KEY);
 
     private static final String FUNDACCOUNT_ID = "fa_Aa00000000001";
-
+    
+    /**
+     * Fund account is created using the customer and item details.
+     * @throws RazorpayException
+     */
     @Test
     public void create() throws RazorpayException {
         JSONObject request = new JSONObject("{\n  \"customer_id\":\"cust_Aa000000000001\",\n  \"account_type\":\"bank_account\",\n  \"bank_account\":{\n    \"name\":\"Gaurav Kumar\",\n    \"account_number\":\"11214311215411\",\n    \"ifsc\":\"HDFC0000053\"\n  }\n}");
-        String json = "{\n  \"id\":\"fa_Aa00000000001\",\n  \"entity\":\"fund_account\",\n  \"customer_id\":\"cust_Aa000000000001\",\n  \"account_type\":\"bank_account\",\n  \"bank_account\":{\n    \"name\":\"Gaurav Kumar\",\n    \"account_number\":\"11214311215411\",\n    \"ifsc\":\"HDFC0000053\",\n    \"bank_name\":\"HDFC Bank\"\n  },\n  \"active\":true,\n  \"created_at\":1543650891\n}";
+        String mockedResponseJson = "{\n  \"id\":\"fa_Aa00000000001\",\n  \"entity\":\"fund_account\",\n  \"customer_id\":\"cust_Aa000000000001\",\n  \"account_type\":\"bank_account\",\n  \"bank_account\":{\n    \"name\":\"Gaurav Kumar\",\n    \"account_number\":\"11214311215411\",\n    \"ifsc\":\"HDFC0000053\",\n    \"bank_name\":\"HDFC Bank\"\n  },\n  \"active\":true,\n  \"created_at\":1543650891\n}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             FundAccount fundaccount = fundAccountClient.create(request);
             assertNotNull(fundaccount);
             assertEquals(FUNDACCOUNT_ID,fundaccount.get("id"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
-
+    
+    /**
+     * Retrieve all fund account of respective customer using customer id.
+     * @throws RazorpayException
+     */
     @Test
     public void fetch() throws RazorpayException {
 
-        String json = "{\n  \"id\":\"fa_Aa00000000001\",\n  \"entity\":\"fund_account\",\n  \"customer_id\":\"cust_Aa000000000001\",\n  \"account_type\":\"bank_account\",\n  \"bank_account\":{\n    \"name\":\"Gaurav Kumar\",\n    \"account_number\":\"11214311215411\",\n    \"ifsc\":\"HDFC0000053\",\n    \"bank_name\":\"HDFC Bank\"\n  },\n  \"active\":true,\n  \"created_at\":1543650891\n}";
+        String mockedResponseJson = "{\n  \"id\":\"fa_Aa00000000001\",\n  \"entity\":\"fund_account\",\n  \"customer_id\":\"cust_Aa000000000001\",\n  \"account_type\":\"bank_account\",\n  \"bank_account\":{\n    \"name\":\"Gaurav Kumar\",\n    \"account_number\":\"11214311215411\",\n    \"ifsc\":\"HDFC0000053\",\n    \"bank_name\":\"HDFC Bank\"\n  },\n  \"active\":true,\n  \"created_at\":1543650891\n}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             FundAccount fetch = fundAccountClient.fetch(FUNDACCOUNT_ID);
             assertNotNull(fetch);
             assertEquals(FUNDACCOUNT_ID,fetch.get("id"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
 }
