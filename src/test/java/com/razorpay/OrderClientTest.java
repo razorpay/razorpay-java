e380f145c681990a0b00ac94b363ef949da5d881
ankitdas13 <ankit.das@razorpay.com>
2022-02-14 13:52:59 +0530

    add javadoc and exception

--- src/test/java/com/razorpay/OrderClientTest.java
+++ src/test/java/com/razorpay/OrderClientTest.java
@@ -16,75 +16,106 @@ public class OrderClientTest extends BaseTest{
 
     private static final String ORDER_ID = "order_EKwxwAgItmmXdp";
 
+    /**
+     * Create order with basic details such as currency and amount details
+     * @throws RazorpayException
+     */
     @Test
     public void create() throws RazorpayException {
         JSONObject request = new JSONObject("{amount:50000,currency:\"INR\",receipt:\"receipt#1\",notes:{key1:\"value3\",key2:\"value2\"}}");
-        String json = "{\"id\":\"order_EKwxwAgItmmXdp\",\"entity\":\"order\",\"amount\":50000,\"amount_paid\":0,\"amount_due\":50000,\"currency\":\"INR\",\"receipt\":\"receipt#1\",\"offer_id\":null,\"status\":\"created\",\"attempts\":0,\"notes\":[],\"created_at\":1582628071}";
+        String mockedResponseJson = "{\"id\":\"order_EKwxwAgItmmXdp\",\"entity\":\"order\",\"amount\":50000,\"amount_paid\":0,\"amount_due\":50000,\"currency\":\"INR\",\"receipt\":\"receipt#1\",\"offer_id\":null,\"status\":\"created\",\"attempts\":0,\"notes\":[],\"created_at\":1582628071}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             Order fetch = orderClient.create(request);
             assertNotNull(fetch);
             assertEquals(ORDER_ID,fetch.get("id"));
+            assertEquals("order",fetch.get("entity"));
+            assertTrue(fetch.has("amount"));
+            assertTrue(fetch.has("amount_paid"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
 
+    /**
+     * Details of all the order can be retrieved.
+     * @throws RazorpayException
+     */
     @Test
     public void fetchAll() throws RazorpayException{
-        String json = "{\"entity\":\"collection\",\"count\":1,\"items\":[{\"id\":\"order_EKzX2WiEWbMxmx\",\"entity\":\"order\",\"amount\":1234,\"amount_paid\":0,\"amount_due\":1234,\"currency\":\"INR\",\"receipt\":\"ReceiptNo.1\",\"offer_id\":null,\"status\":\"created\",\"attempts\":0,\"notes\":[],\"created_at\":1582637108}]}";
+        String mockedResponseJson = "{\"entity\":\"collection\",\"count\":1,\"items\":[{\"id\":\"order_EKzX2WiEWbMxmx\",\"entity\":\"order\",\"amount\":1234,\"amount_paid\":0,\"amount_due\":1234,\"currency\":\"INR\",\"receipt\":\"ReceiptNo.1\",\"offer_id\":null,\"status\":\"created\",\"attempts\":0,\"notes\":[],\"created_at\":1582637108}]}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             List<Order> fetch = orderClient.fetchAll();
             assertNotNull(fetch);
-            assertEquals(true,fetch.get(0).has("amount"));
+            assertTrue(fetch.get(0).has("entity"));
+            assertTrue(fetch.get(0).has("amount"));
+            assertTrue(fetch.get(0).has("amount_paid"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
 
+    /**
+     * Retrieve the order details using order id.
+     * @throws RazorpayException
+     */
     @Test
     public void fetch() throws RazorpayException{
-        String json = "{\"id\":\"order_DaaS6LOUAASb7Y\",\"entity\":\"order\",\"amount\":2200,\"amount_paid\":0,\"amount_due\":2200,\"currency\":\"INR\",\"receipt\":\"Receipt#211\",\"status\":\"attempted\",\"attempts\":1,\"notes\":[],\"created_at\":1572505143}";
+        String mockedResponseJson = "{\"id\":"+ORDER_ID+",\"entity\":\"order\",\"amount\":2200,\"amount_paid\":0,\"amount_due\":2200,\"currency\":\"INR\",\"receipt\":\"Receipt#211\",\"status\":\"attempted\",\"attempts\":1,\"notes\":[],\"created_at\":1572505143}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             Order fetch = orderClient.fetch(ORDER_ID);
             assertNotNull(fetch);
             assertEquals(true,fetch.has("id"));
+            assertTrue(fetch.has("entity"));
+            assertTrue(fetch.has("amount"));
+            assertTrue(fetch.has("amount_paid"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
 
+    /**
+     * Retrieve the payment details of respective customer using payment id.
+     * @throws RazorpayException
+     */
     @Test
     public void fetchPayments() throws RazorpayException{
-        String json = "{\"entity\":\"collection\",\"count\":1,\"items\":[{\"id\":\"pay_DaaSOvhgcOfzgR\",\"entity\":\"payment\",\"amount\":2200,\"currency\":\"INR\",\"status\":\"captured\",\"order_id\":\"order_DaaS6LOUAASb7Y\",\"invoice_id\":null,\"international\":false,\"method\":\"card\",\"amount_refunded\":0,\"refund_status\":null,\"captured\":true,\"description\":\"Beansineveryimaginableflavour\",\"card_id\":\"card_DZon6fd8J3IcA2\",\"bank\":null,\"wallet\":null,\"vpa\":null,\"email\":\"gaurav.kumar@example.com\",\"contact\":\"+919999999988\",\"notes\":[],\"fee\":44,\"tax\":0,\"error_code\":null,\"error_description\":null,\"created_at\":1572505160}]}";
+        String mockedResponseJson = "{\"entity\":\"collection\",\"count\":1,\"items\":[{\"id\":\"pay_DaaSOvhgcOfzgR\",\"entity\":\"payment\",\"amount\":2200,\"currency\":\"INR\",\"status\":\"captured\",\"order_id\":\"order_DaaS6LOUAASb7Y\",\"invoice_id\":null,\"international\":false,\"method\":\"card\",\"amount_refunded\":0,\"refund_status\":null,\"captured\":true,\"description\":\"Beansineveryimaginableflavour\",\"card_id\":\"card_DZon6fd8J3IcA2\",\"bank\":null,\"wallet\":null,\"vpa\":null,\"email\":\"gaurav.kumar@example.com\",\"contact\":\"+919999999988\",\"notes\":[],\"fee\":44,\"tax\":0,\"error_code\":null,\"error_description\":null,\"created_at\":1572505160}]}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             List<Payment> fetch = orderClient.fetchPayments(ORDER_ID);
             assertNotNull(fetch);
-            assertEquals(true,fetch.get(0).has("id"));
+            assertTrue(fetch.get(0).has("id"));
+            assertTrue(fetch.get(0).has("amount"));
+            assertTrue(fetch.get(0).has("currency"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
 
+    /**
+     * Update an order of respective customer using order id with object of that properties
+     * @throws RazorpayException
+     */
     @Test
     public void edit() throws RazorpayException {
         JSONObject request = new JSONObject("{\"notes\":{\"key1\":\"value3\",\"key2\":\"value2\"}}");
-        String json = "{\"id\":\"order_EKwxwAgItmmXdp\",\"entity\":\"order\",\"amount\":2200,\"amount_paid\":0,\"amount_due\":2200,\"currency\":\"INR\",\"receipt\":\"Receipt#211\",\"offer_id\":null,\"status\":\"attempted\",\"attempts\":1,\"notes\":{\"notes_key_1\":\"Tea,EarlGrey,Hot\",\"notes_key_2\":\"Tea,EarlGrey…decaf.\"},\"created_at\":1572505143}";
+        String mockedResponseJson = "{\"id\":"+ORDER_ID+",\"entity\":\"order\",\"amount\":2200,\"amount_paid\":0,\"amount_due\":2200,\"currency\":\"INR\",\"receipt\":\"Receipt#211\",\"offer_id\":null,\"status\":\"attempted\",\"attempts\":1,\"notes\":{\"notes_key_1\":\"Tea,EarlGrey,Hot\",\"notes_key_2\":\"Tea,EarlGrey…decaf.\"},\"created_at\":1572505143}";
         try {
-            mockResponseFromExternalClient(json);
+            mockResponseFromExternalClient(mockedResponseJson);
             mockResponseHTTPCodeFromExternalClient(200);
             Order fetch = orderClient.edit(ORDER_ID,request);
             assertNotNull(fetch);
             assertEquals(ORDER_ID,fetch.get("id"));
+            assertEquals(ORDER_ID,fetch.get("id"));
         } catch (IOException e) {
-            e.printStackTrace();
+            assertTrue(false);
         }
     }
 }
