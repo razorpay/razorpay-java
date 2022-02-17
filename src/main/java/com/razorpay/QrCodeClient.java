a60502e8f1ecdf229c6daa044572afab30000b0b
ankitdas13 <ankit.das@razorpay.com>
2022-02-17 17:18:25 +0530

    implement qr_code with unit test

--- src/main/java/com/razorpay/QrCodeClient.java
+++ src/main/java/com/razorpay/QrCodeClient.java
@@ -19,13 +19,26 @@ public class QrCodeClient extends ApiClient {
     public QrCode fetch(String id) throws RazorpayException {
         return get(String.format(Constants.QRCODE_FETCH, id), null);
     }
-    
+
+    /**
+     * It is wrapper of fetchAll with parameter here sending null defines fetchAll
+     * with a default values without filteration
+     * @throws RazorpayException
+     */
     public List<QrCode> fetchAll() throws RazorpayException {
         return fetchAll(null);
     }
 
+    /**
+     * This method get list of QrCodes filtered by parameters @request
+     * @throws RazorpayException
+     */
     public List<QrCode> fetchAll(JSONObject request) throws RazorpayException {
        return getCollection(Constants.QRCODE_LIST, request);
+    }
+
+    public List<QrCode> fetchAllPayments(String id) throws RazorpayException {
+        return fetchAllPayments(id,null);
     }
 
     public List<QrCode> fetchAllPayments(String id,JSONObject request) throws RazorpayException {
