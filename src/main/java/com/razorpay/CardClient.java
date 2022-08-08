e0879c859bbedcface285d1012009d7906666207
ankitdas13 <ankit.das@razorpay.com>
2022-08-08 16:54:00 +0530

    card api

--- src/main/java/com/razorpay/CardClient.java
+++ src/main/java/com/razorpay/CardClient.java
@@ -1,16 +1,22 @@
 package com.razorpay;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class CardClient extends ApiClient {
 
-  CardClient(String auth) {
-    super(auth);
-  }
+    CardClient(String auth) {
+        super(auth);
+    }
 
-  public Card fetch(String id) throws RazorpayException {
-    return get(String.format(Constants.CARD_GET, id), null);
-  }
+    public Card fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        System.out.println("card hello");
+        return get(String.format(Constants.CARD_GET, id), null);
+    }
 
-  public Card fetchCardDetails(String id) throws RazorpayException{
-    return get(String.format(Constants.FETCH_CARD_DETAILS, id), null);
-  }
+    public Card fetchCardDetails(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return get(String.format(Constants.FETCH_CARD_DETAILS, id), null);
+    }
 }
