b8680d60f804290f89dd578d1e3402b54e8e3e1e
ankitdas13 <ankit.das@razorpay.com>
2022-08-08 17:38:47 +0530

    paymentlink api

--- src/main/java/com/razorpay/PaymentLinkClient.java
+++ src/main/java/com/razorpay/PaymentLinkClient.java
@@ -1,10 +1,11 @@
 package com.razorpay;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
-
-import okhttp3.Response;
 
 public class PaymentLinkClient extends ApiClient {
 
@@ -12,31 +13,31 @@ public class PaymentLinkClient extends ApiClient {
         super(auth);
     }
 
-    public PaymentLink create(JSONObject request) throws RazorpayException {
+    public PaymentLink create(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return post(Constants.PAYMENTLINK_CREATE, request);
     }
-    
-    public PaymentLink fetch(String id) throws RazorpayException {
+
+    public PaymentLink fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return get(String.format(Constants.PAYMENTLINK_GET, id), null);
     }
-    
-    public List<PaymentLink> fetchAll() throws RazorpayException {
+
+    public List<PaymentLink> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
         return fetchAll(null);
     }
 
-    public List<PaymentLink> fetchAll(JSONObject request) throws RazorpayException {
+    public List<PaymentLink> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return getCollection(Constants.PAYMENTLINK_LIST, request);
     }
 
-    public PaymentLink cancel(String id) throws RazorpayException {
+    public PaymentLink cancel(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return post(String.format(Constants.PAYMENTLINK_CANCEL, id), null);
     }
 
-    public PaymentLink edit(String id, JSONObject request) throws RazorpayException {
+    public PaymentLink edit(String id, JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return patch(String.format(Constants.PAYMENTLINK_EDIT, id), request);
     }
 
-    public PaymentLink notifyBy(String id, String medium) throws RazorpayException {
+    public PaymentLink notifyBy(String id, String medium) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return post(String.format(Constants.PAYMENTLINK_NOTIFYBY, id, medium), null);
     }
 }
