6e68be08ab7a7c7f3bffa0af8160369a3ba3ab3f
ankitdas13 <ankit.das@razorpay.com>
2022-08-08 09:49:13 +0530

    fund account api

--- src/main/java/com/razorpay/FundAccountClient.java
+++ src/main/java/com/razorpay/FundAccountClient.java
@@ -1,32 +1,35 @@
 package com.razorpay;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 public class FundAccountClient extends ApiClient{
 
-	FundAccountClient(String auth) {
-		super(auth);
-	}
+    FundAccountClient(String auth) {
+        super(auth);
+    }
 
-	public FundAccount create(JSONObject request) throws RazorpayException {
-	    return post(Constants.FUND_ACCOUNT_CREATE, request);
-	}
+    public FundAccount create(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(Constants.FUND_ACCOUNT_CREATE, request);
+    }
 
-	public FundAccount fetch(String id) throws RazorpayException {
-	    return get(String.format(Constants.FUND_ACCOUNT_FETCH, id), null);
-	}
+    public FundAccount fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return get(String.format(Constants.FUND_ACCOUNT_FETCH, id), null);
+    }
 
-	public List<FundAccount> fetchAll() throws RazorpayException {
-		return fetchAll(null);
-	}
+    public List<FundAccount> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return fetchAll(null);
+    }
 
-	/**
-	 * This method get list of fundaccounts filtered by parameters @request
-	 * @throws RazorpayException
-	 */
-	public List<FundAccount> fetchAll(JSONObject request) throws RazorpayException {
-		return getCollection(Constants.FUND_ACCOUNT_LIST, request);
-	}
+    /**
+     * This method get list of fundaccounts filtered by parameters @request
+     * @throws RazorpayException
+     */
+    public List<FundAccount> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return getCollection(Constants.FUND_ACCOUNT_LIST, request);
+    }
 }
