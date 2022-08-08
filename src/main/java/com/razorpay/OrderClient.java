ec61e1d676797ced9e4a042b98f0da14b4266058
ankitdas13 <ankit.das@razorpay.com>
2022-08-08 16:31:30 +0530

    order api

--- src/main/java/com/razorpay/OrderClient.java
+++ src/main/java/com/razorpay/OrderClient.java
@@ -1,36 +1,39 @@
 package com.razorpay;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class OrderClient extends ApiClient {
 
-  OrderClient(String auth) {
-    super(auth);
-  }
+    OrderClient(String auth) {
+        super(auth);
+    }
 
-  public Order create(JSONObject request) throws RazorpayException {
-    return post(Constants.ORDER_CREATE, request);
-  }
+    public Order create(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(Constants.ORDER_CREATE, request);
+    }
 
-  public List<Order> fetchAll() throws RazorpayException {
-    return fetchAll(null);
-  }
+    public List<Order> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return fetchAll(null);
+    }
 
-  public List<Order> fetchAll(JSONObject request) throws RazorpayException {
-    return getCollection(Constants.ORDER_LIST, request);
-  }
+    public List<Order> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return getCollection(Constants.ORDER_LIST, request);
+    }
 
-  public Order fetch(String id) throws RazorpayException {
-    return get(String.format(Constants.ORDER_GET, id), null);
-  }
+    public Order fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return get(String.format(Constants.ORDER_GET, id), null);
+    }
 
-  public List<Payment> fetchPayments(String id) throws RazorpayException {
-    return getCollection(String.format(Constants.ORDER_PAYMENT_LIST, id), null);
-  }
+    public List<Payment> fetchPayments(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return getCollection(String.format(Constants.ORDER_PAYMENT_LIST, id), null);
+    }
 
-  public Order edit(String id, JSONObject request) throws RazorpayException {
-    return patch(String.format(Constants.ORDER_EDIT, id), request);
-  }
+    public Order edit(String id, JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return patch(String.format(Constants.ORDER_EDIT, id), request);
+    }
 }
