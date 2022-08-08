60912cbca9f001bd573add510822199ecdb0d4e4
ankitdas13 <ankit.das@razorpay.com>
2022-08-08 16:20:49 +0530

    invoice api

--- src/main/java/com/razorpay/InvoiceClient.java
+++ src/main/java/com/razorpay/InvoiceClient.java
@@ -1,52 +1,55 @@
 package com.razorpay;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class InvoiceClient extends ApiClient {
 
-  InvoiceClient(String auth) {
-    super(auth);
-  }
+    InvoiceClient(String auth) {
+        super(auth);
+    }
 
-  public Invoice create(JSONObject request) throws RazorpayException {
-    return post(Constants.INVOICE_CREATE, request);
-  }
+    public Invoice create(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(Constants.INVOICE_CREATE, request);
+    }
 
-  public List<Invoice> fetchAll() throws RazorpayException {
-    return fetchAll(null);
-  }
+    public List<Invoice> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return fetchAll(null);
+    }
 
-  public List<Invoice> fetchAll(JSONObject request) throws RazorpayException {
-    return getCollection(Constants.INVOICE_LIST, request);
-  }
+    public List<Invoice> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return getCollection(Constants.INVOICE_LIST, request);
+    }
 
-  public Invoice fetch(String id) throws RazorpayException {
-    return get(String.format(Constants.INVOICE_GET, id), null);
-  }
+    public Invoice fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return get(String.format(Constants.INVOICE_GET, id), null);
+    }
 
-  public Invoice cancel(String id) throws RazorpayException {
-    return post(String.format(Constants.INVOICE_CANCEL, id), null);
-  }
+    public Invoice cancel(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(String.format(Constants.INVOICE_CANCEL, id), null);
+    }
 
-  public Invoice notifyBy(String id, String medium) throws RazorpayException {
-    return post(String.format(Constants.INVOICE_NOTIFY, id, medium), null);
-  }
+    public Invoice notifyBy(String id, String medium) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(String.format(Constants.INVOICE_NOTIFY, id, medium), null);
+    }
 
-  public Invoice createRegistrationLink(JSONObject request) throws RazorpayException {
-    return post(Constants.SUBSCRIPTION_REGISTRATION_LINK, request);
-  }
+    public Invoice createRegistrationLink(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(Constants.SUBSCRIPTION_REGISTRATION_LINK, request);
+    }
 
-  public Invoice issue(String id) throws RazorpayException {
-    return post(String.format(Constants.INVOICE_ISSUE, id), null);
-  }
+    public Invoice issue(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(String.format(Constants.INVOICE_ISSUE, id), null);
+    }
 
-  public Invoice edit(String id, JSONObject request) throws RazorpayException {
-    return patch(String.format(Constants.INVOICE_GET, id), request);
-  }
+    public Invoice edit(String id, JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return patch(String.format(Constants.INVOICE_GET, id), request);
+    }
 
-  public List<Invoice> delete(String id) throws RazorpayException {
-    return delete(String.format(Constants.INVOICE_GET, id), null);
-  }
+    public List<Invoice> delete(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return delete(String.format(Constants.INVOICE_GET, id), null);
+    }
 }
