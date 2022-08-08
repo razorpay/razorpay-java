b776be881cdea51cdc6f8b51e072278fa2034e89
ankitdas13 <ankit.das@razorpay.com>
2022-08-08 17:14:48 +0530

    plan api

--- src/main/java/com/razorpay/PlanClient.java
+++ src/main/java/com/razorpay/PlanClient.java
@@ -1,7 +1,10 @@
 package com.razorpay;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class PlanClient extends ApiClient {
@@ -10,19 +13,19 @@ public class PlanClient extends ApiClient {
         super(auth);
     }
 
-    public Plan create(JSONObject request) throws RazorpayException {
+    public Plan create(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return post(Constants.PLAN_CREATE, request);
     }
 
-    public Plan fetch(String id) throws RazorpayException {
+    public Plan fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return get(String.format(Constants.PLAN_GET, id), null);
     }
 
-    public List<Plan> fetchAll() throws RazorpayException {
+    public List<Plan> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
         return fetchAll(null);
     }
 
-    public List<Plan> fetchAll(JSONObject request) throws RazorpayException {
+    public List<Plan> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
         return getCollection(Constants.PLAN_LIST, request);
     }
 }
