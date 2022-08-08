9459d6eafef7f2d55513c9491e2cfa4d468b0d07
ankitdas13 <ankit.das@razorpay.com>
2022-08-08 17:44:33 +0530

    transfer api

--- src/main/java/com/razorpay/TransferClient.java
+++ src/main/java/com/razorpay/TransferClient.java
@@ -1,36 +1,39 @@
 package com.razorpay;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class TransferClient extends ApiClient {
 
-  TransferClient(String auth) {
-    super(auth);
-  }
+    TransferClient(String auth) {
+        super(auth);
+    }
 
-  public Transfer create(JSONObject request) throws RazorpayException {
-    return post(Constants.TRANSFER_CREATE, request);
-  }
+    public Transfer create(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(Constants.TRANSFER_CREATE, request);
+    }
 
-  public Transfer edit(String id, JSONObject request) throws RazorpayException {
-    return patch(String.format(Constants.TRANSFER_EDIT, id), request);
-  }
+    public Transfer edit(String id, JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return patch(String.format(Constants.TRANSFER_EDIT, id), request);
+    }
 
-  public Reversal reversal(String id, JSONObject request) throws RazorpayException {
-    return post(String.format(Constants.TRANSFER_REVERSAL_CREATE, id), request);
-  }
+    public Reversal reversal(String id, JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return post(String.format(Constants.TRANSFER_REVERSAL_CREATE, id), request);
+    }
 
-  public Transfer fetch(String id) throws RazorpayException {
-    return get(String.format(Constants.TRANSFER_GET, id), null);
-  }
+    public Transfer fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return get(String.format(Constants.TRANSFER_GET, id), null);
+    }
 
-  public List<Transfer> fetchAll() throws RazorpayException {
-    return fetchAll(null);
-  }
+    public List<Transfer> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return fetchAll(null);
+    }
 
-  public List<Transfer> fetchAll(JSONObject request) throws RazorpayException {
-    return getCollection(Constants.TRANSFER_LIST, request);
-  }
+    public List<Transfer> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return getCollection(Constants.TRANSFER_LIST, request);
+    }
 }
