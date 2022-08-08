d2f5ea62aa3846f8031fac4552b9ace39e6eb667
ankitdas13 <ankit.das@razorpay.com>
2022-08-08 17:30:18 +0530

    addon api

--- src/main/java/com/razorpay/AddonClient.java
+++ src/main/java/com/razorpay/AddonClient.java
@@ -1,38 +1,41 @@
 package com.razorpay;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class AddonClient extends ApiClient {
 
-  AddonClient(String auth) {
-    super(auth);
-  }
-
-  // To create an Addon, use the createAddon method of SubscriptionClient
-  public Addon fetch(String id) throws RazorpayException {
-    return get(String.format(Constants.ADDON_GET, id), null);
-  }
-  
-  /**
-   * It is wrapper of fetchAll with parameter here sending null defines fetchAll
-   * with a default values without filteration
-   * @throws RazorpayException
-   */
-  public List<Addon> fetchAll() throws RazorpayException {
-      return fetchAll(null);
-    }
-  
-  /**
-   * This method get list of Addons filtered by parameters @request
-   * @throws RazorpayException
-   */  
-  public List<Addon> fetchAll(JSONObject request) throws RazorpayException {
-      return getCollection(Constants.ADDON_LIST, request);
+    AddonClient(String auth) {
+        super(auth);
     }
 
-  public List<Addon> delete(String id) throws RazorpayException {
-    return delete(String.format(Constants.ADDON_DELETE, id), null);
-  }
+    // To create an Addon, use the createAddon method of SubscriptionClient
+    public Addon fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return get(String.format(Constants.ADDON_GET, id), null);
+    }
+
+    /**
+     * It is wrapper of fetchAll with parameter here sending null defines fetchAll
+     * with a default values without filteration
+     * @throws RazorpayException
+     */
+    public List<Addon> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return fetchAll(null);
+    }
+
+    /**
+     * This method get list of Addons filtered by parameters @request
+     * @throws RazorpayException
+     */
+    public List<Addon> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return getCollection(Constants.ADDON_LIST, request);
+    }
+
+    public List<Addon> delete(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
+        return delete(String.format(Constants.ADDON_DELETE, id), null);
+    }
 }
