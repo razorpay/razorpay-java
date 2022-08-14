1394c5f70c8fc7dd60a79bd3d66d5e6fd2822fb6
ankitdas13 <ankit.das@razorpay.com>
2022-08-14 12:01:50 +0530

    fundaccount entity and mock

--- src/test/java/com/razorpay/FundAccountClientTest.java
+++ src/test/java/com/razorpay/FundAccountClientTest.java
@@ -1,26 +1,32 @@
 package com.razorpay;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class FundAccountClientTest extends BaseTest{
 
-    @InjectMocks
-    protected FundAccountClient fundAccountClient = new FundAccountClient(TEST_SECRET_KEY);
+    @Mock
+    ApiUtils apiUtils;
 
     private static final String FUNDACCOUNT_ID = "fa_Aa00000000001";
-    
+
     /**
      * Fund account is created using the customer and item details.
      * @throws RazorpayException
      */
     @Test
-    public void create() throws RazorpayException {
+    public void create() throws RazorpayException, JSONException, URISyntaxException {
         JSONObject request = new JSONObject("{\n  " +
                 "\"customer_id\":\"cust_Aa000000000001\",\n" +
                 "\"account_type\":\"bank_account\",\n" +
@@ -43,24 +49,27 @@ public class FundAccountClientTest extends BaseTest{
                 "\"active\":true,\n" +
                 "\"created_at\":1543650891\n}";
         try {
-            mockResponseFromExternalClient(mockedResponseJson);
-            mockResponseHTTPCodeFromExternalClient(200);
+            apiUtils = mock(ApiUtils.class);
+            URL builder = ApiClient.getBuilder(Constants.FUND_ACCOUNT_CREATE, null);
+            mockPostRequest(apiUtils,builder,request.toString(),mockedResponseJson);
+            FundAccountClient fundAccountClient = new FundAccountClient("test",apiUtils);
+
             FundAccount fundaccount = fundAccountClient.create(request);
             assertNotNull(fundaccount);
             assertEquals(FUNDACCOUNT_ID,fundaccount.get("id"));
-            String createRequest = getHost(Constants.FUND_ACCOUNT_CREATE);
-            verifySentRequest(true, request.toString(), createRequest);
+
+
         } catch (IOException e) {
             assertTrue(false);
         }
     }
-    
+
     /**
      * Retrieve all fund account of respective customer using customer id.
      * @throws RazorpayException
      */
     @Test
-    public void fetch() throws RazorpayException {
+    public void fetch() throws RazorpayException, JSONException, URISyntaxException {
 
         String mockedResponseJson = "{\n" +
                 "\"id\":\"fa_Aa00000000001\",\n" +
@@ -76,13 +85,15 @@ public class FundAccountClientTest extends BaseTest{
                 "\"active\":true,\n" +
                 "\"created_at\":1543650891\n}";
         try {
-            mockResponseFromExternalClient(mockedResponseJson);
-            mockResponseHTTPCodeFromExternalClient(200);
+            apiUtils = mock(ApiUtils.class);
+            URL builder = ApiClient.getBuilder(String.format(Constants.FUND_ACCOUNT_FETCH, FUNDACCOUNT_ID), null);
+            mockGetRequest(apiUtils,builder,null,mockedResponseJson);
+
+            FundAccountClient fundAccountClient = new FundAccountClient("test",apiUtils);
             FundAccount fetch = fundAccountClient.fetch(FUNDACCOUNT_ID);
             assertNotNull(fetch);
             assertEquals(FUNDACCOUNT_ID,fetch.get("id"));
-            String fetchRequest = getHost(String.format(Constants.FUND_ACCOUNT_FETCH,FUNDACCOUNT_ID));
-            verifySentRequest(false, null, fetchRequest);
+
         } catch (IOException e) {
             assertTrue(false);
         }
