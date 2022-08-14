dc7d3973729700c6a34a706169db21926a6af2d0
ankitdas13 <ankit.das@razorpay.com>
2022-08-14 13:57:48 +0530

    refund entity and mock

--- src/main/java/com/razorpay/RefundClient.java
+++ src/main/java/com/razorpay/RefundClient.java
@@ -1,40 +1,43 @@
 package com.razorpay;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RefundClient extends ApiClient {
 
-  RefundClient(String auth) {
-    super(auth);
+  RefundClient(String auth, ApiUtils apiUtils) {
+    super(auth,apiUtils);
   }
 
-  public Refund create(JSONObject request) throws RazorpayException {
+  public Refund create(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return post(Constants.REFUNDS, request);
   }
 
-  public List<Refund> fetchAll(JSONObject request) throws RazorpayException {
+  public List<Refund> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return getCollection(Constants.REFUNDS, request);
   }
 
-  public Refund fetch(String id) throws RazorpayException {
+  public Refund fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return get(String.format(Constants.REFUND, id), null);
   }
 
-  public List<Refund> fetchAll() throws RazorpayException {
+  public List<Refund> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
     return fetchAll(null);
   }
 
-  public List<Refund> fetchMultipleRefund(String id) throws RazorpayException {
+  public List<Refund> fetchMultipleRefund(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return fetchMultipleRefund(id,null);
   }
 
-  public List<Refund> fetchMultipleRefund(String id,JSONObject request) throws RazorpayException {
+  public List<Refund> fetchMultipleRefund(String id,JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return getCollection(String.format(Constants.REFUND_MULTIPLE, id), request);
   }
 
-  public Refund edit(String id, JSONObject request) throws RazorpayException {
+  public Refund edit(String id, JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return patch(String.format(Constants.REFUND, id), request);
   }
 }
