43cc17efc9efa2144f01854ed56fe1bbe506d7b6
ankitdas13 <ankit.das@razorpay.com>
2022-08-14 11:50:27 +0530

    customer entity and mocktest

--- src/main/java/com/razorpay/CustomerClient.java
+++ src/main/java/com/razorpay/CustomerClient.java
@@ -1,24 +1,27 @@
 package com.razorpay;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CustomerClient extends ApiClient {
 
-  CustomerClient(String auth) {
-    super(auth);
+  CustomerClient(String auth, ApiUtils apiUtils) {
+    super(auth,apiUtils);
   }
 
-  public Customer create(JSONObject request) throws RazorpayException {
+  public Customer create(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return post(Constants.CUSTOMER_CREATE, request);
   }
 
-  public Customer fetch(String id) throws RazorpayException {
+  public Customer fetch(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return get(String.format(Constants.CUSTOMER_GET, id), null);
   }
 
-  public Customer edit(String id, JSONObject request) throws RazorpayException {
+  public Customer edit(String id, JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return put(String.format(Constants.CUSTOMER_EDIT, id), request);
   }
 
@@ -27,7 +30,7 @@ public class CustomerClient extends ApiClient {
    * with a default values without filteration
    * @throws RazorpayException
    */
-  public List<Customer> fetchAll() throws RazorpayException {
+  public List<Customer> fetchAll() throws RazorpayException, JSONException, IOException, URISyntaxException {
     return fetchAll(null);
   }
 
@@ -35,19 +38,19 @@ public class CustomerClient extends ApiClient {
    * This method get list of customers filtered by parameters @request
    * @throws RazorpayException
    */
-  public List<Customer> fetchAll(JSONObject request) throws RazorpayException {
+  public List<Customer> fetchAll(JSONObject request) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return getCollection(Constants.CUSTOMER_LIST, request);
   }
 
-  public List<Token> fetchTokens(String id) throws RazorpayException {
+  public List<Token> fetchTokens(String id) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return getCollection(String.format(Constants.TOKEN_LIST, id), null);
   }
 
-  public Token fetchToken(String id, String tokenId) throws RazorpayException {
+  public Token fetchToken(String id, String tokenId) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return get(String.format(Constants.TOKEN_GET, id, tokenId), null);
   }
 
-  public Customer deleteToken(String id, String tokenId) throws RazorpayException {
+  public Customer deleteToken(String id, String tokenId) throws RazorpayException, JSONException, IOException, URISyntaxException {
     return delete(String.format(Constants.TOKEN_DELETE, id, tokenId), null);
   }
 }
