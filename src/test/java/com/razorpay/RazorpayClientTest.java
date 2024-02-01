c7608cbdd1cbe3b962bc320147f255102165ebdb
Bhavya Y <bhavya.y@razorpay.com>
2024-02-01 17:34:29 +0530

    Update RazorpayClientTest.java

--- src/test/java/com/razorpay/RazorpayClientTest.java
+++ src/test/java/com/razorpay/RazorpayClientTest.java
@@ -17,8 +17,12 @@ public class RazorpayClientTest extends BaseTest {
     @InjectMocks
     protected RazorpayClient razorpayClient;
 
+    @InjectMocks
+    protected RazorpayClient instance;
+
     {
         try {
+            instance = new RazorpayClient("test", "secret");
             razorpayClient = new RazorpayClient(TEST_SECRET_KEY);
         } catch (RazorpayException e) {
             throw new RuntimeException(e);
@@ -55,12 +59,50 @@ public class RazorpayClientTest extends BaseTest {
             assertNotNull(fetch);
             assertEquals("order_EKwxwAgItmmXdp",fetch.get("id"));
             assertEquals("order",fetch.get("entity"));
-            assertTrue(fetch.has("amount"));
-            assertTrue(fetch.has("amount_paid"));
             String createRequest = getHost(Constants.ORDER_CREATE);
 
             Map<String, String> headers = new HashMap<>();
             headers.put(Constants.AUTH_HEADER_KEY, "Bearer " + TEST_SECRET_KEY);
+            verifySendRequestAndHeaders(request.toString(), createRequest, headers);
+        } catch (IOException | RazorpayException e) {
+            assertTrue(false);
+        }
+    }
+
+    @Test
+    public void testOrderCreationWithApiKeyAndSecret() {
+        JSONObject request = new JSONObject("{" +
+                "amount:50000," +
+                "currency:\"INR\"," +
+                "receipt:\"receipt#1\"," +
+                "notes:" +
+                "{key1:\"value3\"," +
+                "key2:\"value2\"}}");
+
+        String mockedResponseJson = "{" +
+                "\"id\":\"order_EKwxwAgItmmXdp\"," +
+                "\"entity\":\"order\"," +
+                "\"amount\":50000," +
+                "\"amount_paid\":0," +
+                "\"amount_due\":50000," +
+                "\"currency\":\"INR\"," +
+                "\"receipt\":\"receipt#1\"," +
+                "\"offer_id\":null," +
+                "\"status\":\"created\"," +
+                "\"attempts\":0," +
+                "\"notes\":[]," +
+                "\"created_at\":1582628071}";
+        try {
+            mockResponseFromExternalClient(mockedResponseJson);
+            mockResponseHTTPCodeFromExternalClient(200);
+            Order fetch = instance.orders.create(request);
+            assertNotNull(fetch);
+            assertEquals("order_EKwxwAgItmmXdp",fetch.get("id"));
+            assertEquals("order",fetch.get("entity"));
+            String createRequest = getHost(Constants.ORDER_CREATE);
+
+            Map<String, String> headers = new HashMap<>();
+            headers.put(Constants.AUTH_HEADER_KEY, "Basic dGVzdDpzZWNyZXQ=");
             verifySendRequestAndHeaders(request.toString(), createRequest, headers);
         } catch (IOException | RazorpayException e) {
             assertTrue(false);
