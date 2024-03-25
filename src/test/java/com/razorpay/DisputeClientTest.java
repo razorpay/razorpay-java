46a881a06c6136a4ee915aeb9e36d424a3462f7a
ankitdas13 <ankit.das@razorpay.com>
2024-03-25 12:29:33 +0530

    change json to pojo

--- src/test/java/com/razorpay/DisputeClientTest.java
+++ src/test/java/com/razorpay/DisputeClientTest.java
@@ -1,10 +1,12 @@
 package com.razorpay;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.*;
 
@@ -22,67 +24,77 @@ public class DisputeClientTest extends BaseTest{
     @Test
     public void TestContest() throws RazorpayException{
 
-        JSONObject request = new JSONObject("{\n" +
-                "  \"amount\": 5000,\n" +
-                "  \"summary\": \"goods delivered\",\n" +
-                "  \"shipping_proof\": [\n" +
-                "    \"doc_EFtmUsbwpXwBH9\",\n" +
-                "    \"doc_EFtmUsbwpXwBH8\"\n" +
-                "  ],\n" +
-                "  \"others\": [\n" +
-                "    {\n" +
-                "      \"type\": \"receipt_signed_by_customer\",\n" +
-                "      \"document_ids\": [\n" +
-                "        \"doc_EFtmUsbwpXwBH1\",\n" +
-                "        \"doc_EFtmUsbwpXwBH7\"\n" +
-                "      ]\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"action\": \"draft\"\n" +
-                "}");
+        JSONObject request = new JSONObject();
+        request.put("amount",5000);
+        request.put("summary","goods delivered");
+        List<Object> shipping_proof = new ArrayList<>();
+        shipping_proof.add("doc_EFtmUsbwpXwBH9");
+        shipping_proof.add("doc_EFtmUsbwpXwBH8");
+        request.put("shipping_proof", shipping_proof);
+        List<Object> others = new ArrayList<>();
+        JSONObject otherParam = new JSONObject();
+        otherParam.put("type","receipt_signed_by_customer");
+        List<Object> doc = new ArrayList<>();
+        doc.add("doc_EFtmUsbwpXwBH1");
+        doc.add("doc_EFtmUsbwpXwBH7");
+        otherParam.put("document_ids",doc);
+        others.add(otherParam);
+        request.put("others", others);
+        request.put("action", "submit");
 
-        String mockedResponseJson = "{\n" +
-                "  \"id\": \"disp_AHfqOvkldwsbqt\",\n" +
-                "  \"entity\": \"dispute\",\n" +
-                "  \"payment_id\": \"pay_EsyWjHrfzb59eR\",\n" +
-                "  \"amount\": 10000,\n" +
-                "  \"currency\": \"INR\",\n" +
-                "  \"amount_deducted\": 0,\n" +
-                "  \"reason_code\": \"chargeback\",\n" +
-                "  \"respond_by\": 1590604200,\n" +
-                "  \"status\": \"open\",\n" +
-                "  \"phase\": \"chargeback\",\n" +
-                "  \"created_at\": 1590059211,\n" +
-                "  \"evidence\": {\n" +
-                "    \"amount\": 5000,\n" +
-                "    \"summary\": \"goods delivered\",\n" +
-                "    \"shipping_proof\": [\n" +
-                "      \"doc_EFtmUsbwpXwBH9\",\n" +
-                "      \"doc_EFtmUsbwpXwBH8\"\n" +
-                "    ],\n" +
-                "    \"billing_proof\": null,\n" +
-                "    \"cancellation_proof\": null,\n" +
-                "    \"customer_communication\": null,\n" +
-                "    \"proof_of_service\": null,\n" +
-                "    \"explanation_letter\": null,\n" +
-                "    \"refund_confirmation\": null,\n" +
-                "    \"access_activity_log\": null,\n" +
-                "    \"refund_cancellation_policy\": null,\n" +
-                "    \"term_and_conditions\": null,\n" +
-                "    \"others\": [\n" +
-                "      {\n" +
-                "        \"type\": \"receipt_signed_by_customer\",\n" +
-                "        \"document_ids\": [\n" +
-                "          \"doc_EFtmUsbwpXwBH1\",\n" +
-                "          \"doc_EFtmUsbwpXwBH7\"\n" +
-                "        ]\n" +
-                "      }\n" +
-                "    ],\n" +
-                "    \"submitted_at\": null\n" +
-                "  }\n" +
-                "}";
+        JSONObject mockedResponseJson = new JSONObject();
+
+        mockedResponseJson.put("id", "disp_AHfqOvkldwsbqt");
+        mockedResponseJson.put("amount", 5000);
+        mockedResponseJson.put("entity", "dispute");
+        mockedResponseJson.put("summary", "goods delivered");
+
+        JSONArray shippingProofArray = new JSONArray();
+        shippingProofArray.put("doc_EFtmUsbwpXwBH9");
+        shippingProofArray.put("doc_EFtmUsbwpXwBH8");
+        mockedResponseJson.put("shipping_proof", shippingProofArray);
+
+        mockedResponseJson.put("billing_proof", JSONObject.NULL);
+        mockedResponseJson.put("cancellation_proof", JSONObject.NULL);
+        mockedResponseJson.put("customer_communication", JSONObject.NULL);
+        mockedResponseJson.put("proof_of_service", JSONObject.NULL);
+        mockedResponseJson.put("explanation_letter", JSONObject.NULL);
+        mockedResponseJson.put("refund_confirmation", JSONObject.NULL);
+        mockedResponseJson.put("access_activity_log", JSONObject.NULL);
+        mockedResponseJson.put("refund_cancellation_policy", JSONObject.NULL);
+        mockedResponseJson.put("term_and_conditions", JSONObject.NULL);
+
+        JSONArray othersArray = new JSONArray();
+        JSONObject othersObject = new JSONObject();
+        othersObject.put("type", "receipt_signed_by_customer");
+        JSONArray documentIdsArray = new JSONArray();
+        documentIdsArray.put("doc_EFtmUsbwpXwBH1");
+        documentIdsArray.put("doc_EFtmUsbwpXwBH7");
+        othersObject.put("document_ids", documentIdsArray);
+        othersArray.put(othersObject);
+        mockedResponseJson.put("others", othersArray);
+
+        mockedResponseJson.put("submitted_at", JSONObject.NULL);
+
+        JSONObject evidenceObject = new JSONObject();
+        evidenceObject.put("amount", 5000);
+        evidenceObject.put("summary", "goods delivered");
+        evidenceObject.put("shipping_proof", shippingProofArray);
+        evidenceObject.put("billing_proof", JSONObject.NULL);
+        evidenceObject.put("cancellation_proof", JSONObject.NULL);
+        evidenceObject.put("customer_communication", JSONObject.NULL);
+        evidenceObject.put("proof_of_service", JSONObject.NULL);
+        evidenceObject.put("explanation_letter", JSONObject.NULL);
+        evidenceObject.put("refund_confirmation", JSONObject.NULL);
+        evidenceObject.put("access_activity_log", JSONObject.NULL);
+        evidenceObject.put("refund_cancellation_policy", JSONObject.NULL);
+        evidenceObject.put("term_and_conditions", JSONObject.NULL);
+        evidenceObject.put("others", othersArray);
+        evidenceObject.put("submitted_at", JSONObject.NULL);
+        mockedResponseJson.put("evidence", evidenceObject);
+
         try {
-            mockResponseFromExternalClient(mockedResponseJson);
+            mockResponseFromExternalClient(mockedResponseJson.toString());
             mockResponseHTTPCodeFromExternalClient(200);
             Dispute dispute = disputeClient.contest(DISPUTE_ID,request);
             assertNotNull(dispute);
